167c38512c702f45614bb2754148f184b44ce245
Dominik Zappe <dominikzappe1999@seznam.cz>
2022-03-05 18:08:56 +0000

    Updated visuals Zoom, radius, z, center stays with updating data Animation frame stays with updated data Bootstrap added Added buttons and sliders for params

--- visuals.py
+++ visuals.py
@@ -1,8 +1,10 @@
 import plotly.express as px
 import pandas as pd
-from dash import Dash, dcc, html, Input, Output
+import dash
+from dash import dcc, html, Input, Output, State
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div([
     html.Div(
@@ -11,15 +13,16 @@ app.layout = html.Div([
     html.Div(
         children=[
             html.Div("'Z' scale coefficient", style={"text-align": "center", "margin-bottom": "10px"}),
-            dcc.Slider(1, 20, .1, value=8, id='z-slider', marks=None,
+            dcc.Slider(1, 20, .1, value=8, id='z-slider', marks={i: f'{i}' for i in range(1, 21)},
                        tooltip={"placement": "bottom", "always_visible": True}),
             html.Div("Radius scale coefficient", style={"text-align": "center", "margin-bottom": "10px",
                                                         "margin-top": "50px"}),
-            dcc.Slider(1, 20, .1, value=2.5, id='radius-slider', marks=None,
+            dcc.Slider(1, 20, .1, value=2.5, id='radius-slider', marks={i: f'{i}' for i in range(1, 21)},
                        tooltip={"placement": "bottom", "always_visible": True}),
-            html.Div(html.Button("Update data", id="update-button", n_clicks=0,
-                                 style={'font-size': '12px', 'width': '140px', 'display': 'inline-block',
-                                        'height': '40px'}),
+            html.Div(children=[dbc.Button("Update data", id="update-button", n_clicks=0, outline=True, color="dark",
+                                          size="lg", style={"margin-right": "100px"}),
+                               dbc.Button("Reset", id="reset-button", n_clicks=0, outline=True, color="danger",
+                                          size="lg")],
                      style={"margin-top": "550px", "justify-content": "center", "align-items": "center",
                             "display": "flex"})
         ],
@@ -28,11 +31,25 @@ app.layout = html.Div([
 
 
 @app.callback(
-    Output('map', 'figure'),
+    [Output('map', 'figure'),
+     Output("z-slider", "value"),
+     Output("radius-slider", "value")],
     Input('update-button', 'n_clicks'),
+    Input("reset-button", "n_clicks"),
     Input("z-slider", "value"),
-    Input("radius-slider", "value"),)
-def update_figure(unused_input, z_coef, radius_coef):
+    Input("radius-slider", "value"),
+    State("map", "figure"))
+def update_figure(unused_input, reset_input, z_coef, radius_coef, curr_fig):
+    ctx = dash.callback_context
+
+    trigger = None
+    if ctx.triggered:
+        trigger = ctx.triggered[0]["prop_id"]
+
+    if trigger is not None and "reset" in trigger:
+        z_coef = 8
+        radius_coef = 2.5
+
     df = pd.read_csv("data/merged.csv")
 
     fig = px.density_mapbox(df, lat="latitude", lon="longitude", z=df["pocet_obyvatel"] ** (1.0 / z_coef),
@@ -42,7 +59,23 @@ def update_figure(unused_input, z_coef, radius_coef):
                             center=dict(lat=49.88537, lon=15.3684), zoom=6.6, color_continuous_scale="plasma")
     fig.update_layout(hoverlabel=dict(bgcolor="white", font_size=16, font_family="Inter"))
 
-    return fig
+    if curr_fig is not None and trigger is not None and not ("reset" in trigger):
+        fig["layout"]["sliders"][0]["active"] = curr_fig["layout"]["sliders"][0]["active"]
+        fig["data"][0].coloraxis = curr_fig["data"][0]["coloraxis"]
+        fig["data"][0].customdata = curr_fig["data"][0]["customdata"]
+        fig["data"][0].hovertemplate = curr_fig["data"][0]["hovertemplate"]
+        fig["data"][0].hovertext = curr_fig["data"][0]["hovertext"]
+        fig["data"][0].lat = curr_fig["data"][0]["lat"]
+        fig["data"][0].lon = curr_fig["data"][0]["lon"]
+        fig["data"][0].name = curr_fig["data"][0]["name"]
+        if "update" in trigger:
+            fig["data"][0].radius = curr_fig["data"][0]["radius"]
+            fig["data"][0].z = curr_fig["data"][0]["z"]
+        fig["data"][0].subplot = curr_fig["data"][0]["subplot"]
+        fig["layout"]["mapbox"]["center"] = curr_fig["layout"]["mapbox"]["center"]
+        fig["layout"]["mapbox"]["zoom"] = curr_fig["layout"]["mapbox"]["zoom"]
+
+    return [fig, z_coef, radius_coef]
 
 
 if __name__ == '__main__':
